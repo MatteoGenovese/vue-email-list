b8429f48b3f65a76c5cd2cf3c87765bc9774d08d
MatteoGenovese <matteo.genovese@icloud.com>
2022-07-13 16:02:27 +0200

    Milestone

--- js/main.js
+++ js/main.js
@@ -3,10 +3,33 @@ var app = new Vue({
 
     data: {
 
+        contacts: [],
+
+
     },
 
     methods: {
 
+        getContact: function(indexPassed) {
 
-    }
+            return this.contacts[indexPassed];
+
+        }
+
+
+
+
+    },
+
+    created() {
+
+        for (let index = 0; index < 10; index++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    this.contacts.push(response.data.response)
+                    console.log(response.data.response);
+                })
+        }
+
+    },
 });
