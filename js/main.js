0e010ab6b6cc707e9e6fd4f565a632e169f3e768
MatteoGenovese <matteo.genovese@icloud.com>
2022-07-13 16:19:11 +0200

    Bonus

--- js/main.js
+++ js/main.js
@@ -2,34 +2,30 @@ var app = new Vue({
     el: '#app',
 
     data: {
-
         contacts: [],
-
-
     },
 
     methods: {
-
         getContact: function(indexPassed) {
-
             return this.contacts[indexPassed];
-
+        },
+        generateNewEmails: function() {
+            for (let index = 0; index < 10; index++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        this.contacts.push(response.data.response)
+                        console.log(response.data.response);
+                    })
+            }
+        },
+        generationHasFinished: function() {
+            if (this.contacts.length == 10)
+                return true;
+            else
+                return false;
         }
-
-
-
-
     },
-
     created() {
-
-        for (let index = 0; index < 10; index++) {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((response) => {
-                    this.contacts.push(response.data.response)
-                    console.log(response.data.response);
-                })
-        }
-
+        this.generateNewEmails();
     },
 });
